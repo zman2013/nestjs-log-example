d3eebe34078b3a006db05171e3a32b8ec0ce8516
zman <manzhiyuan@lixiang.com>
2021-04-12 22:13:51 +0800

    use nestjs-log in nestjs project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getLog } from 'nestjs-log'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: getLog('system').loggerService
+  });
   await app.listen(3000);
 }
 bootstrap();
